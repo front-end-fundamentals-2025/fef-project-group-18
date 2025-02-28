0c8249d4519c4c436a261c66eea603f2df337b08
VedangC-JU <chve24wm@student.ju.se>
2025-02-28 06:23:14 +0100

    Fixing the problem with menu toggle button

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav");
+    const navMenu = document.querySelector("nav ul");
 
     menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
+        navMenu.classList.toggle("show-menu");
     });
 
     document.addEventListener("click", function (event) {
         if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
-            navMenu.classList.remove("show");
+            navMenu.classList.remove("show-menu");
         }
     });
 });
